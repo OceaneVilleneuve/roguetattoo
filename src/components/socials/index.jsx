193ea1e4ae6b9c9b5d85117af2dbbabc299c2b07
Oceane Villeneuve <oceanevilleneuve16@gmail.com>
2025-05-25 20:21:53 +0200

    editing my profil and logo

--- src/components/socials/index.jsx
+++ src/components/socials/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import Logo from "../../assets/IMG_0751-removebg-preview.png";
+import Logo from "../../assets/IMG_3075.PNG";
 import { useMediaQuery } from "react-responsive";
 // import colors from '../../utils/style/colors';
 
